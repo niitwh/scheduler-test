ce3bccc01d6593d25ccc46940b14e7c752f11365
niitwh <46929322+niitwh@users.noreply.github.com>
2022-09-01 17:15:06 +0800

    Update sample_plugin.go

--- pkg/plugins/sample_plugin.go
+++ pkg/plugins/sample_plugin.go
@@ -5,6 +5,7 @@ import (
 	"hash/crc32"
 	"math/rand"
 	"time"
+	"context"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -16,7 +17,7 @@ import (
 const Name = "sample-plugin"
 
 type Sample struct {
-	handle framework.FrameworkHandle
+	handle framework.Handle
 }
 
 func (s *Sample) Name() string {
@@ -37,37 +38,37 @@ func getNodeNameHash(s string) int {
 }
 
 
-func (s *Sample) PreFilter(pc *framework.PluginContext, pod *v1.Pod) *framework.Status {
+
+func (s *Sample) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
 	klog.V(3).Infof("prefilter pod: %v", pod.Name)
-	return framework.NewStatus(framework.Success, "")
+	return framework.NewStatus(framework.Success, "")	
 }
 
-func (s *Sample) Filter(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
-	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
+
+func (s *Sample) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeInfo.Node().Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
 // score based on node name
-func Score(pc *framework.PluginContext, p *v1.Pod, nodeName string) (int, *framework.Status) {
+func (s *Sample) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
 	hashValue := getNodeNameHash(nodeName) % 100
 	rand.Seed(time.Now().UnixNano())
-	score := (hashValue + rand.Intn(100)) % 100
+	score := (int64)(hashValue + rand.Intn(100)) % 100
 	klog.V(3).Infof("node: %v, score: %d", nodeName, score)
 	return score, framework.NewStatus(framework.Success, "")
 }
 
 
-func (s *Sample) PreBind(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
-	if nodeInfo, ok := s.handle.NodeInfoSnapshot().NodeInfoMap[nodeName]; !ok {
-		return framework.NewStatus(framework.Error, fmt.Sprintf("prebind get node info error: %+v", nodeName))
-	} else {
-		klog.V(3).Infof("prebind node info: %+v", nodeInfo.Node())
-		return framework.NewStatus(framework.Success, "")
-	}
+func (s *Sample) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+	klog.V(3).Infof("prebind node info: %v", nodeName)
+	return framework.NewStatus(framework.Success, "")
 }
 
+
+
 //type PluginFactory = func(f FrameworkHandle) (Plugin, error)
-func New(configuration *runtime.Unknown, f framework.FrameworkHandle) (framework.Plugin, error) {
+func New(f framework.Handle) (framework.Plugin, error) {
 	return &Sample{
 		handle: f,
 	}, nil
